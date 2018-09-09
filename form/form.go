fac71b189a5cad9c42a20c327466cf0e04bead9f
lpuigo <laurent.puig@gmail.com>
2018-09-09 16:43:40 +0200

    Initial commit

---
+++ form/form.go
@@ -0,0 +1,104 @@
+package form
+
+import "fmt"
+
+type FormModel struct {
+	Categories []struct {
+		Key           string        `json:"Key"`
+		Title         string        `json:"Title"`
+		SubCategories []SubCategory `json:"SubCategories"`
+	} `json:"Categories"`
+	ValidationRules []struct {
+		Rule          string `json:"Rule"`
+		ErrorMessage  string `json:"ErrorMessage"`
+		RedirectField string `json:"RedirectField"`
+	} `json:"ValidationRules"`
+}
+
+type SubCategory struct {
+	Key        string  `json:"Key"`
+	Title      string  `json:"Title"`
+	Visibility string  `json:"Visibility,omitempty"`
+	Fields     []Field `json:"Fields"`
+}
+
+func (s SubCategory) String() string {
+	return fmt.Sprintf("%s\n", s.Title)
+}
+
+type Field struct {
+	Ref             string   `json:"Ref"`                       // Référence permettant d'identifier un champ
+	Type            string   `json:"Type"`                      // Type du champ
+	Label           string   `json:"Label"`                     // Libellé associé au champ
+	Label2          string   `json:"Label2,omitempty"`          // Libellé associé au champ (utilisé pour certains champs
+	Label3          string   `json:"Label3,omitempty"`          // Libellé associé au champ (utilisé pour certains champs
+	Visibility      string   `json:"Visibility,omitempty"`      // Règle de visibilité permettant d'afficher/masquer le champ
+	Tooltip         string   `json:"Tooltip,omitempty"`         // Affiche un \"?\" en bout de ligne et affiche le contenu dans un tooltip au click du \"?\"\"
+	Placeholder     string   `json:"Placeholder,omitempty"`     // Valeur affiché par défaut (valable que pour les champs libres de type text)
+	IsMandatory     bool     `json:"IsMandatory,omitempty"`     // Interdit que le champ soit vide (affiche une *)
+	IsMandatoryExpr string   `json:"IsMandatoryExpr,omitempty"` // Idem, mais sur la base d'une expression à évaluer
+	MessErrRequired string   `json:"MessErrRequired,omitempty"` // Message d'erreur affiché si le champ est vide
+	IsReadonly      bool     `json:"IsReadonly"`                // Le champ n'est pas modifiable
+	Href            string   `json:"Href,omitempty"`            // Lien hypertext pour les champs de type link ou linkAppli
+	Regexp          string   `json:"Regexp,omitempty"`          // Règle de validation des champs texte sous forme d'expression régulière
+	MessErrRegexp   string   `json:"MessErrRegexp,omitempty"`   // Message d'erreur affiché si le champ ne respecte pas Regexp
+	SectionTitle    string   `json:"SectionTitle,omitempty"`    // Affiche un titre au dessus d'un champ. La règle de visibilité n'affecte pas le titre de la section.
+	DefaultValue    struct { // Description d'une valeur par défaut. Une seule valeur possible (oneOf pas supporté par swagger v2).
+		Static                   string `json:"Static,omitempty"`                   // Valeur en dur dans le format attendu par le type du champ
+		OriginatorWorkOrderQuery string `json:"OriginatorWorkOrderQuery,omitempty"` // Requête xpath 1.0 appliqué sur l'OT brut provenant du canal donneur d'ordre
+		WorkOrderQuery           string `json:"WorkOrderQuery,omitempty"`           // Requête jsonpath appliqué sur l'OT Planning. Voir le format de l'objet retourné par ApiWorkOrderByIdGetAsync
+	} `json:"DefaultValue,omitempty"`
+	Quality struct { // Information de classification qualité du champ (non géré par le moteur de rendu)
+		Ref string `json:"Ref"`
+	} `json:"Quality,omitempty"`
+	PidiLineTestType      []int      `json:"PidiLineTestType,omitempty"`      // Permet de sélectionner les essais mis à disposition
+	CatalogServiceFilter  string     `json:"CatalogServiceFilter,omitempty"`  // Concerne le type catalogInput. Valeur du filtre affecté par défaut (code famille de prestation)
+	CatalogMaterialFilter string     `json:"CatalogMaterialFilter,omitempty"` // Concerne le type catalogInput. Valeur du filtre affecté par défaut (code famille de matériel)
+	CatalogPriceDisplay   bool       `json:"CatalogPriceDisplay,omitempty"`   // Concerne le type catalogInput. Permet d'indiquer si les prix totaux sont affichés dans le composant côté application mobile
+	CatalogSummary        []string   `json:"CatalogSummary,omitempty"`        // Concerne le type catalogInput. Permet d'indiquer la référence des champs dont on souhaite faire le résumé
+	OptionList            []struct { // Pour les types choix, dresse la liste des choix possibles
+		Visibility string `json:"Visibility,omitempty"` // Règle de visibilité permettant d'afficher/masquer le groupe d'option
+		Choices    []struct {
+			Value   string `json:"Value"`
+			Display string `json:"Display"`
+		} `json:"Choices"`
+	} `json:"OptionList,omitempty"`
+	CascadeOptionList []struct { // Pour les types choix dans les listes en cascade, dresse la liste des choix possibles
+		Value   string   `json:"Value"`
+		Display string   `json:"Display"`
+		Items   []string `json:"Items,omitempty"`
+	} `json:"CascadeOptionList,omitempty"`
+	Fields   []Field `json:"Fields,omitempty"`   // Pour le type itemlist, dresse la liste des champs groupés et répétables
+	MaxItem  int     `json:"MaxItem,omitempty"`  // Nombre maximum d'élément dans le groupe de champs itemlist
+	MinLevel int     `json:"MinLevel,omitempty"` // niveau technicien minimum inclus nécessaire pour voir le champs
+	MaxLevel int     `json:"MaxLevel,omitempty"` // niveau technicien minimum inclus nécessaire pour voir le champs
+}
+
+type FieldType string
+
+const (
+	FT_address            FieldType = "address"
+	FT_booleanInput       FieldType = "booleanInput"
+	FT_cascadeChoice      FieldType = "cascadeChoice"
+	FT_dateInput          FieldType = "dateInput"
+	FT_dateTimeInput      FieldType = "dateTimeInput"
+	FT_gpsInput           FieldType = "gpsInput"
+	FT_itemList           FieldType = "itemList"
+	FT_link               FieldType = "link"
+	FT_linkAppli          FieldType = "linkAppli"
+	FT_mailInput          FieldType = "mailInput"
+	FT_multipleChoice     FieldType = "multipleChoice"
+	FT_numInput           FieldType = "numInput"
+	FT_photoInput         FieldType = "photoInput"
+	FT_pjInput            FieldType = "pjInput"
+	FT_qrcodeInput        FieldType = "qrcodeInput"
+	FT_signatureInput     FieldType = "signatureInput"
+	FT_singleChoice       FieldType = "singleChoice"
+	FT_singleInput        FieldType = "singleInput"
+	FT_telInput           FieldType = "telInput"
+	FT_text               FieldType = "text"
+	FT_timeInput          FieldType = "timeInput"
+	FT_pidiLineTestsInput FieldType = "pidiLineTestsInput"
+	FT_catalogInput       FieldType = "catalogInput"
+	FT_tamTestsInput      FieldType = "tamTestsInput"
+)
---
+++ form/form_test.go
@@ -0,0 +1,32 @@
+package form
+
+import (
+	"encoding/json"
+	"os"
+	"testing"
+)
+
+const Form1 string = `C:\Users\Laurent\Golang\src\github.com\lpuig\scopelecformulairecontrol\form\test\PCCDGT.json`
+
+func Test_FormMarshall(t *testing.T) {
+	f, err := os.Open(Form1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+
+	var myForm FormModel
+
+	err = json.NewDecoder(f).Decode(&myForm)
+	if err != nil {
+		t.Fatal("could not decode form", err)
+	}
+
+	for ic, c := range myForm.Categories {
+		t.Logf("%d: %s [%s]\n", ic, c.Title, c.Key)
+		for isc, s := range c.SubCategories {
+			t.Logf("\t%d-%d: %s\n", ic, isc, s.String())
+		}
+	}
+
+}
