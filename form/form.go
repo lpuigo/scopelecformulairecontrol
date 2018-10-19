e4338fc516b58abfcd453eedb81edc77ce699e43
lpuigo <laurent.puig@gmail.com>
2018-10-20 00:21:04 +0200

    Add style update

--- form/form.go
+++ form/form.go
@@ -7,6 +7,7 @@ import (
 	"github.com/tealeg/xlsx"
 	"io"
 	"os"
+	"strings"
 )
 
 type FormModel struct {
@@ -156,9 +157,9 @@ func mandatory(b bool) string {
 func readonly(b bool) string {
 	switch b {
 	case true:
-		return "L"
+		return "Lect.Seule"
 	default:
-		return "L+E"
+		return "Lect.Ecrit"
 	}
 }
 
@@ -221,8 +222,83 @@ func (f FormModel) WriteCSV(w io.Writer) {
 	}
 }
 
-func xlsHeader(s *xlsx.Sheet) {
-	xlsRow(s,
+func (f FormModel) WriteXLS(w io.Writer) error {
+	xs, err := NewXLSSheet()
+	if err != nil {
+		return err
+	}
+	xs.xlsHeader()
+
+	for ic, c := range f.Categories {
+		xs.xlsRow(fmt.Sprintf("%d", ic),
+			"Categorie", c.Key, c.Title,
+			"",
+			"",
+			"",
+			"",
+			"",
+		)
+		xs.rowColor("00000000", "0000b6ff")
+		for isc, s := range c.SubCategories {
+			xs.xlsRow(fmt.Sprintf("%d-%d", ic, isc),
+				"Sous-Categorie", s.Key, s.Title,
+				"",
+				"",
+				"",
+				s.Visibility,
+				"",
+			)
+			xs.rowColor("00000000", "0051cdff")
+
+			for ifield, field := range s.Fields {
+				xs.xlsRow(fmt.Sprintf("%d-%d-%d", ic, isc, ifield),
+					field.Type, field.Ref, field.Label,
+					mandatory(field.IsMandatory),
+					readonly(field.IsReadonly),
+					field.SectionTitle,
+					field.Visibility,
+					field.DefaultValue.String(),
+				)
+				if strings.HasPrefix(field.Ref, "PIDI_") {
+					xs.r.Cells[2].GetStyle().Font.Bold = true
+				}
+				if field.IsMandatory {
+					xs.cellColor(4, "00a80000")
+				}
+				if field.IsReadonly {
+					xs.cellColor(5, "00a80000")
+				}
+			}
+		}
+	}
+
+	err = xs.f.Write(w)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+type XLSSheet struct {
+	f *xlsx.File
+	s *xlsx.Sheet
+	r *xlsx.Row
+}
+
+func NewXLSSheet() (xs *XLSSheet, err error) {
+	xs = &XLSSheet{
+		f: xlsx.NewFile(),
+	}
+	xlsx.SetDefaultFont(11, "Calibri")
+	xs.s, err = xs.f.AddSheet("Form")
+	if err != nil {
+		return
+	}
+	return
+}
+
+func (xs *XLSSheet) xlsHeader() {
+	xs.xlsRow(
 		"Position",
 		"Type",
 		"Ref",
@@ -233,58 +309,49 @@ func xlsHeader(s *xlsx.Sheet) {
 		"VisibilityRule",
 		"DefaultValue",
 	)
+
+	styles := []struct {
+		width    float64
+		autowrap bool
+	}{
+		{8, false},
+		{17, false},
+		{43, false},
+		{50, true},
+		{12, false},
+		{11, false},
+		{30, true},
+		{50, true},
+		{50, true},
+	}
+
+	for i, c := range xs.s.Cols {
+		c.Width = styles[i].width
+		if styles[i].autowrap {
+			cs := c.GetStyle()
+			cs.Alignment = xlsx.Alignment{WrapText: true}
+			cs.ApplyAlignment = true
+		}
+	}
 }
 
-func xlsRow(s *xlsx.Sheet, pos, typ, ref, label, mandatory, readonly, section, visibility, defaultvalue string) {
-	r := s.AddRow()
+func (xs *XLSSheet) xlsRow(pos, typ, ref, label, mandatory, readonly, section, visibility, defaultvalue string) {
+	xs.r = xs.s.AddRow()
 	for _, str := range []string{pos, typ, ref, label, mandatory, readonly, section, visibility, defaultvalue} {
-		c := r.AddCell()
+		c := xs.r.AddCell()
 		c.SetString(str)
 	}
 }
 
-func (f FormModel) WriteXLS(w io.Writer) error {
-	xlsFile := xlsx.NewFile()
-	sheet, err := xlsFile.AddSheet("Form")
-	if err != nil {
-		return err
+func (xs *XLSSheet) rowColor(fgcolor, bgcolor string) {
+	for _, c := range xs.r.Cells {
+		s := c.GetStyle()
+		s.Fill = *xlsx.NewFill("solid", bgcolor, "00000000")
+		s.ApplyFill = true
+		s.Font.Color = fgcolor
 	}
-	xlsHeader(sheet)
+}
 
-	for ic, c := range f.Categories {
-		xlsRow(sheet, fmt.Sprintf("%d", ic),
-			"Categorie", c.Key, c.Title,
-			"",
-			"",
-			"",
-			"",
-			"",
-		)
-		for isc, s := range c.SubCategories {
-			xlsRow(sheet, fmt.Sprintf("%d-%d", ic, isc),
-				"Sous-Categorie", s.Key, s.Title,
-				"",
-				"",
-				"",
-				s.Visibility,
-				"",
-			)
-			for ifield, field := range s.Fields {
-				xlsRow(sheet, fmt.Sprintf("%d-%d-%d", ic, isc, ifield),
-					field.Type, field.Ref, field.Label,
-					mandatory(field.IsMandatory),
-					readonly(field.IsReadonly),
-					field.SectionTitle,
-					field.Visibility,
-					field.DefaultValue.String(),
-				)
-			}
-		}
-	}
-
-	err = xlsFile.Write(w)
-	if err != nil {
-		return err
-	}
-	return nil
+func (xs *XLSSheet) cellColor(num int, fgcolor string) {
+	xs.r.Cells[num].GetStyle().Font.Color = fgcolor
 }
