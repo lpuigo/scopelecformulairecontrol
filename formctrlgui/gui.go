f2848ccb12a0822633e726470c12dec78154a79d
lpuigo <laurent.puig@gmail.com>
2018-10-31 22:25:56 +0100

    Add GUI exec

---
+++ formctrlgui/gui.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"github.com/lpuig/scopelecformulairecontrol/form"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/lxn/walk"
+	. "github.com/lxn/walk/declarative"
+)
+
+type GuiContext struct {
+	textEdit *walk.TextEdit
+}
+
+func main() {
+	gc := GuiContext{}
+	MainWindow{
+		Title:   "SPRINT Formulaire Controler",
+		MinSize: Size{320, 240},
+		Layout:  VBox{},
+		OnDropFiles: func(files []string) {
+			gc.GenXLSX(files)
+		},
+		Children: []Widget{
+			TextEdit{
+				AssignTo:  &gc.textEdit,
+				ReadOnly:  true,
+				Text:      "Glisser un formulaire JSON ici ...",
+				Alignment: AlignCenter,
+			},
+		},
+	}.Run()
+}
+
+func outputfile(filename string) string {
+	return strings.TrimSuffix(filename, filepath.Ext(filename)) + ".xlsx"
+}
+
+func (gc GuiContext) GenXLSX(filenames []string) {
+	for _, filename := range filenames {
+		output := outputfile(filename)
+		err := genXlxsfromForm(filename, output)
+		if err != nil {
+			newText := gc.textEdit.Text() + "\r\nErreur : " + err.Error()
+			gc.textEdit.SetText(newText)
+			continue
+		}
+		newText := gc.textEdit.Text() + "\r\n" + "Résultat : " + output
+		gc.textEdit.SetText(newText)
+	}
+}
+
+func genXlxsfromForm(jsonfile, xlsxfile string) error {
+	model, err := form.NewFormModelFromFile(jsonfile)
+	if err != nil {
+		return fmt.Errorf("impossible de lire le Formulaire : %v", err)
+	}
+
+	outf, err := os.Create(xlsxfile)
+	if err != nil {
+		return fmt.Errorf("impossible de créer le fichier résultat : %v", err)
+	}
+	err = model.WriteXLS(outf)
+	if err != nil {
+		os.Remove(xlsxfile)
+		return fmt.Errorf("impossible d'écrire le fichier XLSX : %v", err)
+	}
+	outf.Close()
+	return nil
+}
